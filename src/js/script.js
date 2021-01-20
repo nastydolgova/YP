813e0df197eadab96b1f47e0746edd2dd2a3e43e
Anastasia D <nasty.dolgova@gmail.com>
2021-01-20 20:44:22 +0300

    gallery from json

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
-let mainNav = document.querySelector('.main-nav');
-let menuBtn = document.querySelector('.main-nav__toogle');
+const mainNav = document.querySelector('.main-nav');
+const menuBtn = document.querySelector('.main-nav__toogle');
+const fotoBox = document.querySelector('.page-main--gallery');
 
 mainNav.classList.remove('main-nav--no-js');
 
@@ -7,6 +8,18 @@ menuBtn.addEventListener('click', openMenu );
 
 function openMenu(){
     mainNav.classList.toggle('main-nav--closed');
-    bgDiv.classList.toggle('hidden');
     menuBtn.classList.toggle('main-nav__toogle--close');
+}
+
+fetch( '../foto.json')
+    .then((res) => res.json())
+    .then(data => setFoto(data));
+
+function setFoto({name, fotos}){
+    fotoBox.insertAdjacentHTML(
+        'afterbegin',
+        `
+         ${fotos.map((f) => `<img class="foto-item" src="${f.urlfoto}">`).join(' ')} 
+        `
+    )
 }
